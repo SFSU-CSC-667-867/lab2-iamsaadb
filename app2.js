0106b28a8c1d4e11a2fd14cfb902228685e424da
Saad <33384287+iamsaadb@users.noreply.github.com>
2019-02-20 14:43:59 -0800

    App1 and app2

--- app2.js
+++ app2.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const axios = require('axios');
 //axios makes network requests
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 //to declare a route you need get. 
 //This can be used to requests from one server to another using ip adresses (or domain names)
-app.get('/Hello', (req, res) => {
-    console.log(req.query);
-    res.send('Hello ' + req.query.name + ' World');
+app.get('/world', (req, res) => {
+   res.send('Hello  World');
 } 
-);
+); 
 
-app.get('/network', (req, res) => {
-   axios.get('http://localhost:3001/world')
-   .then((networkResponse) => 
-    res.send(networkResponse.data)
-   )
-   .catch(() => {
-       res.send('DID NOT WORK');
-   })
-} 
-);
 
 //app.get('/add', (req, res) => res.send('<html><title>THIS IS MY ADD PAGE </title><body><font size="6" color="red" >THOSE WHO LIVE IN GLASS HOUSES SHOULDNT TRY STONES</font></body></html>'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
